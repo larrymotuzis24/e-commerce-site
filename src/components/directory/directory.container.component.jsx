f3e5d1f75d7a2c8e5996815adc2eb01e7a3475c8
Larry Motuzis <mlauris2006@yahoo.com>
2023-10-23 13:52:11 -0500

    updated direcotry component and categoryItem component

---
+++ src/components/directory/directory.container.component.jsx
@@ -0,0 +1,14 @@
+import "./directory.container.styles.scss";
+import Categoryitem from "../category-item/category-item.component";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map(({ id, title, imageUrl }) => {
+        return <Categoryitem key={id} title={title} imageUrl={imageUrl} />;
+      })}
+    </div>
+  );
+};
+
+export default Directory;
